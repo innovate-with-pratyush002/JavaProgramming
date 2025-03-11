c2c0a0b303e728fae03af1ccd5e7f85223176041
PRATYUSH KUMAR NISHAD <harityush2015@gmail.com>
2025-03-11 23:59:05 +0530

    //print fibonacci sequence till n;

---
+++ Recursion/D_recursion3.java
@@ -0,0 +1,23 @@
+//print fibonacci sequence till n;
+import java.util.*;
+public class D_recursion3 {
+    public static void fibonacci(int a,int b,int n){
+        if(n==2){
+            return;
+        }
+        int c=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        fibonacci(b,c,n-1);
+
+      
+    }
+    public static void main(String[] args){
+       Scanner sc= new Scanner(System.in);
+       int n= sc.nextInt();
+       int a=0,b=1;
+
+       fibonacci(a,b,n);
+    }
+}
