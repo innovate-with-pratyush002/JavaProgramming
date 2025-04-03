6c62c3e71093125409248155e7f15e7c8e441598
PRATYUSH KUMAR NISHAD <harityush2015@gmail.com>
2025-04-03 23:31:24 +0530

    parallelogram

---
+++ PatternsQuestion/parallelogram.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class parallelogram{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n:");
+        int n= sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int s=1;s<=n;s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
